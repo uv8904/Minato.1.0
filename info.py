2215957cd24217a5db830f79f9940e6c25fdd324
uv8904 <uvjangra2@gmail.com>
2025-12-12 18:33:02 +0530

    Update group link for the bot

--- info.py
+++ info.py
@@ -120,7 +120,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_isavilable') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_requestss_group') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/hmmmmw876') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/wanda_movies_update') # Update channel link for the bot
 
