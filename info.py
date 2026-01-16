fca1f8fae0a82d518eebeb780e7c1997bc3f60a0
uv8904 <uvjangra2@gmail.com>
2026-01-16 17:58:19 +0530

    Update SUPPORT_CHAT_ID to new default value

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002573823548'))  # Log channel i
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002551533001'))  # Bin channel id (make sure bot is admin)
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002648671971'))  # Premium logs channel id
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002635477308').split()] #(make sure bot is admin)
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002517228726')  # Support group id (make sure bot is admin)
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1003506315650')  # Support group id (make sure bot is admin)
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1003494610843')  # Request channel id (make sure bot is admin)
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/premiumottreleases')  # Support group link (make sure bot is admin)
 
