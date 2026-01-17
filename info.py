b175112ed8fd24b2ca53ff4f2e20384aadd969df
uv8904 <uvjangra2@gmail.com>
2026-01-17 13:45:41 +0530

    Add new channel to AUTH_CHANNELS for force sub

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/premiumottreleases')  #
 
 # FORCE_SUB 
 auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-1002377815135")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
-auth_channels     = environ.get("AUTH_CHANNELS", "-1003140286992 -1002536320197 -1003192727274")# Channels for force sub (make sure bot is admin)
+auth_channels     = environ.get("AUTH_CHANNELS", "-1003140286992 -1002536320197 -1003192727274 -1003696183217")# Channels for force sub (make sure bot is admin)
 
 # ============================
 # Payment Configuration
